3b873d0daf3bcbd3e1bac2713a67a91a41413c05
KKYTester <KANG123KY@GMAIL.COM>
2026-08-26 16:08:21 +0800

    update: chatbot name changed form Duke to Potato

---
+++ src/main/java/Potato.java
@@ -0,0 +1,13 @@
+/**
+ * Starts the Potato chatbot application.
+ */
+public class Potato {
+    public static void main(String[] args) {
+        String banner = "PPPP   OOO  TTTTT  A  TTTTT  OOO \n"
+                + "P   P O   O   T   A A   T   O   O\n"
+                + "PPPP  O   O   T  AAAAA  T   O   O\n"
+                + "P     O   O   T  A   A  T   O   O\n"
+                + "P      OOO    T  A   A  T    OOO \n";
+        System.out.println(banner);
+    }
+}
