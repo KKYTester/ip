5a04a3672d2450da6b7466cddffedb22f207dccc
KKYTester <KANG123KY@GMAIL.COM>
2026-08-26 17:20:38 +0800

    Add Increment: Level-0

--- src/main/java/Potato.java
+++ src/main/java/Potato.java
@@ -2,12 +2,25 @@
  * Starts the Potato chatbot application.
  */
 public class Potato {
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " ____   ___    _____     _     _____   ___\n"
                 + "|  _ \\ / _ \\  |_   _|   / \\   |_   _| / _ \\\n"
                 + "| |_) | | | |   | |    / _ \\    | |  | | | |\n"
                 + "|  __/| |_| |   | |   / ___ \\   | |  | |_| |\n"
                 + "|_|    \\___/    |_|  /_/   \\_\\  |_|   \\___/\n";
-        System.out.println(banner);
+        printGreetingAndExit(banner);
+    }
+
+    private static void printGreetingAndExit(String banner) {
+        String greeting = SEPARATOR + "\n"
+                + banner
+                + "Hello! I'm Potato.\n"
+                + "What can I do for you?\n"
+                + SEPARATOR + "\n"
+                + "Bye. Hope to see you again soon!\n"
+                + SEPARATOR + "\n";
+        System.out.print(greeting);
     }
 }
